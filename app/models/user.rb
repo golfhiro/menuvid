8f694aee509dc28e0eeebd5ed796cac8d836a3a1
golfhiro <hiroki5118@gmail.com>
2023-03-07 11:36:32 +0900

    ユーザー登録一旦完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  validates :name, presence: true, length: {maximum: 20}
+  validates :email, presence: true, length: {maximum: 100}
+  validates :password, length: { minimum: 3 }, confirmation: true
+
 end
