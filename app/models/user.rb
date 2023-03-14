d572a8338ae9db0783b4fa35cd220ce244d285c8
golfhiro <hiroki5118@gmail.com>
2023-03-14 10:27:43 +0900

    ユーザー登録エラーの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: {maximum: 20}, uniqueness: true
   validates :email, presence: true, length: {maximum: 100}, uniqueness: true
-  validates :password, length: { minimum: 3 }, confirmation: true, uniqueness: true
+  validates :password, length: { minimum: 3 }, confirmation: true
 
 end
