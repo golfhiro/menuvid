a1a6e720571420514016de02a8fb58bd76d8e1e1
golfhiro <hiroki5118@gmail.com>
2023-03-17 22:10:35 +0900

    日付と曜日に対して１つの動画を表示

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,6 +2,14 @@ class TopsController < ApplicationController
   include YoutubeApi
 
   def index
-    @videos =  Video.order('RANDOM()').limit(2)
+    @videos =  Video.order('RANDOM()').limit(31)
+
+    @dates = (Date.today.beginning_of_month..Date.today.end_of_month).map { |date| date.strftime('%-m/%-d (%A)') }
+
+    # @dates.each do |date_dish|
+    #   @videos =  Video.order('RANDOM()').limit(31)
+    # end
+
+    # @next_dates = (Date.today.end_of_month.next_day..Date.today.end_of_month.next_month).map { |date| date.strftime('%-m/%-d (%A)')}
   end
 end
