6f2764fed2b0bfcb6e8abac070708d9a6a20a486
golfhiro <hiroki5118@gmail.com>
2023-03-18 15:40:29 +0900

    1週間分の日付と曜日ごとに動画を表示

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,15 +1,15 @@
 class TopsController < ApplicationController
-  include YoutubeApi
+  # include YoutubeApi
 
   def index
-    @videos =  Video.order('RANDOM()').limit(31)
+    @videos =  Video.order('RANDOM()').limit(7)
 
-    @dates = (Date.today.beginning_of_month..Date.today.end_of_month).map { |date| date.strftime('%-m/%-d (%A)') }
+    @dates = (Date.today.beginning_of_week..Date.today.end_of_week).map { |date| date.strftime('%-m/%-d (%A)') }
 
-    # @dates.each do |date_dish|
-    #   @videos =  Video.order('RANDOM()').limit(31)
-    # end
+    #先月の日付と曜日
+    # @last_dates = (Date.today.beginning_of_month.prev_month..Date.today.end_of_month.prev_month).map { |date| date.strftime('%-m/%-d (%A)') }
 
+    #来月の日付と曜日
     # @next_dates = (Date.today.end_of_month.next_day..Date.today.end_of_month.next_month).map { |date| date.strftime('%-m/%-d (%A)')}
   end
 end
