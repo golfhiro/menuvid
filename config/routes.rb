c6adc106ad2823fd064e6ee55c09521aadacab30
golfhiro <hiroki5118@gmail.com>
2023-03-22 17:57:14 +0900

    パスワードリセット機能

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
+  resources :password_resets, only: %i[new create edit update]
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+
 end
