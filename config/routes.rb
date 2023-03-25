54acb0561798b0305242b09f810c6beebd20eb22
golfhiro <hiroki5118@gmail.com>
2023-03-25 16:33:48 +0900

    一旦保存

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  namespace :admin do
-      resources :videos
-      resources :users
-      resources :dishes
-
-      root to: "videos#index"
-    end
   root "tops#index"
 
   resources :users
