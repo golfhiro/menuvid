9a22a69822e06baf9754d36030e33c02b1812576
golfhiro <hiroki5118@gmail.com>
2023-03-27 18:49:50 +0900

    top page、1週間のメニュー作成

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,8 +2,5 @@ class TopsController < ApplicationController
   skip_before_action :require_login
 
   def index
-    @videos =  Video.order('RANDOM()').limit(7)
-
-    @dates = (Date.today.beginning_of_week..Date.today.end_of_week).map { |date| date.strftime('%-m/%-d (%A)') }
   end
 end
