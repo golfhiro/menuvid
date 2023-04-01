fd8cc50ed3bfe4e95a661f30437766ccde27ee51
golfhiro <hiroki5118@gmail.com>
2023-04-01 17:03:00 +0900

    dishesコントローラのリファクタリング

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,7 +1,6 @@
 class DishesController < ApplicationController
   before_action :require_login
   before_action :authenticate_admin
-  # skip_before_action :require_login
 
   require 'google/apis/youtube_v3'
 
@@ -10,36 +9,17 @@ class DishesController < ApplicationController
     @dishes = Dish.all
   end
 
-  def search_videos
+def search_videos
     @dish = Dish.find(params[:dish_id])
 
-    # YouTube APIの呼び出しと、取得した動画をVideoモデルに保存する処理を実装する
-    youtube = Google::Apis::YoutubeV3::YouTubeService.new
-    youtube.key = Rails.application.credentials.youtube_api[:youtube_api_key]
-
-    search_response = youtube.list_searches(
-      'snippet',
-      q: @dish.name + "レシピ",
-      type: 'video',
-      max_results: 1,
-      # 関連度が高い順
-      order: "relevance",
-    ).items
-
-    if search_response.present?
-      video = Video.new(
-        video_id: search_response.first.id.video_id,
-        title: search_response.first.snippet.title,
-        thumbnail: search_response.first.snippet.thumbnails.high.url,
-        dish: @dish # VideoとDishを関連付ける
-      )
-      video.save
-    end
+    Video.search_by_dish(@dish)
 
     redirect_to action: 'index'
   end
 
   def index
+    @dishes = Dish.all
+    @videos = Video.includes(:dish).order('dishes.id ASC')
   end
 
   private
