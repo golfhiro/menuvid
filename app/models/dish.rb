f505cefa90e0ffa9ac3a972462d2819aba9c3557
golfhiro <hiroki5118@gmail.com>
2023-04-13 14:56:58 +0900

    ジャンルと材料モデルの設定

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -3,4 +3,9 @@ class Dish < ApplicationRecord
 
   has_many :menus, dependent: :destroy
   has_one :video, dependent: :destroy
+  has_many :dish_genre_tags, dependent: :destroy
+  has_many :genre_tags, through: :dish_genre_tags
+  has_many :dish_ingredient_tags, dependent: :destroy
+  has_many :ingredient_tags, through: :dish_ingredient_tags
+
 end
--- app/models/genre_tag.rb
+++ app/models/genre_tag.rb
@@ -1,2 +1,6 @@
 class GenreTag < ApplicationRecord
+  has_many :dish_genre_tags, dependent: :destroy
+  has_many :dishes, through: :dish_genre_tags
+
+  validates :name, presence: true
 end
