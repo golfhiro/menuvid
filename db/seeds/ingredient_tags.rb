338966e0af287139fb5edbd089559caf18d18cec
golfhiro <hiroki5118@gmail.com>
2023-04-13 15:15:14 +0900

    タグのseedデータの作成

---
+++ db/seeds/ingredient_tags.rb
@@ -0,0 +1,5 @@
+IngredientTag.find_or_create_by!(name: '牛肉')
+IngredientTag.find_or_create_by!(name: '豚肉')
+IngredientTag.find_or_create_by!(name: '鶏肉')
+IngredientTag.find_or_create_by!(name: '魚')
+IngredientTag.find_or_create_by!(name: '野菜')
