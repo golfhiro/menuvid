169672acc3656cf6ac013967896e50217236ec48
golfhiro <hiroki5118@gmail.com>
2023-04-25 12:39:08 +0900

    CMD [rails, server, -b, 0.0.0.0]の追加

--- Dockerfile
+++ Dockerfile
@@ -50,6 +50,7 @@ COPY entrypoint.sh /usr/bin/
 RUN chmod +x /usr/bin/entrypoint.sh
 ENTRYPOINT ["entrypoint.sh"]
 EXPOSE 3000
+CMD ["rails", "server", "-b", "0.0.0.0"]
 
 
 # FROM ruby:3.1.2
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.hosts << "localhost"
   config.hosts << "menuvid.fly.dev"
 end
