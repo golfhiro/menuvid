d1911a38548475b393d5688a41f3467784e4393e
golfhiro <tamabolt24@gmail.com>
2023-05-20 17:15:22 +0900

    add ingredient_tag

--- db/seeds/ingredient_tags.rb
+++ db/seeds/ingredient_tags.rb
@@ -1,15 +1,31 @@
+#肉
 IngredientTag.find_or_create_by!(name: '牛肉')
 IngredientTag.find_or_create_by!(name: '豚肉')
 IngredientTag.find_or_create_by!(name: '鶏肉')
 IngredientTag.find_or_create_by!(name: '挽肉')
 
+#魚
 IngredientTag.find_or_create_by!(name: '白身魚')
 IngredientTag.find_or_create_by!(name: '赤身魚')
 IngredientTag.find_or_create_by!(name: 'エビ')
 
+#その他
 IngredientTag.find_or_create_by!(name: '豆腐')
+IngredientTag.find_or_create_by!(name: '卵')
 
+#野菜
 IngredientTag.find_or_create_by!(name: '野菜')
+IngredientTag.find_or_create_by!(name: 'キャベツ')
+IngredientTag.find_or_create_by!(name: 'ニラ')
+IngredientTag.find_or_create_by!(name: '玉ねぎ')
+IngredientTag.find_or_create_by!(name: '人参')
+IngredientTag.find_or_create_by!(name: 'じゃがいも')
+IngredientTag.find_or_create_by!(name: 'ピーマン')
+IngredientTag.find_or_create_by!(name: '白菜')
+IngredientTag.find_or_create_by!(name: 'きのこ')
+
+#主食
+IngredientTag.find_or_create_by!(name: '米')
 
 IngredientTag.find_or_create_by!(name: '麺')
-IngredientTag.find_or_create_by!(name: '米')
+IngredientTag.find_or_create_by!(name: '春雨')
