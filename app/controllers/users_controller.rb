f4f0da671ff4c765a27328fcf7b5977cc168270f
golfhiro <tamabolt24@gmail.com>
2023-05-24 16:21:54 +0900

    不要なアクションの削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,6 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
   skip_before_action :require_login
 
-  def index
-    @users = User.all
-  end
-
   def new
     @user = User.new
   end
