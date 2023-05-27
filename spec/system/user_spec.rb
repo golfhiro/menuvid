45ce17cda71841c956e002ea2a6c875c8c79c61e
golfhiro <tamabolt24@gmail.com>
2023-05-27 10:37:32 +0900

    system/user_specの記述

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -1,42 +1,63 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe 'Users', type: :system do
-#   let(:existed_user) { create(:user) }
+RSpec.describe 'Users', type: :system do
+  let(:user) { create(:user) }
 
-#   describe 'ユーザー新規作成' do
-#     context '正常系' do
-#       it '新規作成ができる' do
-#         visit new_user_path
-#         fill_in 'メールアドレス', with: 'test01@example.com'
-#         fill_in 'パスワード', with: 'password'
-#         fill_in 'パスワード（確認）', with: 'password'
-#         click_button '新規登録'
-#         expect(current_path).to eq menus_path
-#       end
-#     end
+  describe 'ユーザー新規登録' do
+    context 'フォームの入力値が正常' do
+      it 'ユーザーの新規作成が成功する' do
+        visit new_user_path
+        fill_in '名前', with: 'user_1'
+        fill_in 'メールアドレス', with: 'test@example.com'
+        fill_in 'パスワード', with: 'パスワード'
+        fill_in 'パスワード（確認）', with: 'パスワード'
+        click_button '新規登録'
+        expect(current_path).to eq users_path
+      end
+    end
 
-#     context '異常系' do
-#       it 'メールアドレスが未入力の時、新規作成ができない' do
-#         visit new_user_path
-#         fill_in 'メールアドレス', with: ''
-#         fill_in 'パスワード', with: 'password'
-#         fill_in 'パスワード（確認）', with: 'password'
-#         click_button '新規登録'
-#         expect(current_path).to eq new_user_path
-#         expect(page).to have_content 'ユーザー登録に失敗しました'
-#         expect(page).to have_content 'メールアドレスを入力してください'
-#       end
+    context '名前が未入力' do
+      it 'ユーザーの新規作成が失敗する' do
+        visit new_user_path
+        fill_in '名前', with: ''
+        fill_in 'メールアドレス', with: 'test@example.com'
+        fill_in 'パスワード', with: 'パスワード'
+        fill_in 'パスワード（確認）', with: 'パスワード'
+        click_button '新規登録'
+        expect(page).to have_content '名前名前を入力してください。'
+        expect(page).to have_content "ユーザー登録に失敗しました"
+        expect(current_path).to eq users_path
+      end
+    end
 
-#       it 'メールアドレスが登録済の時、新規作成ができない' do
-#         visit new_user_path
-#         fill_in 'メールアドレス', with: existed_user.email
-#         fill_in 'パスワード', with: 'password'
-#         fill_in 'パスワード（確認）', with: 'password'
-#         click_button '新規登録'
-#         expect(current_path).to eq new_user_path
-#         expect(page).to have_content 'ユーザー登録に失敗しました'
-#         expect(page).to have_content 'メールアドレスはすでに存在します'
-#       end
-#     end
-#   end
-# end
+    context 'メールアドレスが未入力' do
+      it 'ユーザーの新規作成が失敗する' do
+        visit new_user_path
+        fill_in '名前', with: 'hiroki'
+        fill_in 'メールアドレス', with: ''
+        fill_in 'パスワード', with: 'パスワード'
+        fill_in 'パスワード（確認）', with: 'パスワード'
+        click_button '新規登録'
+        expect(page).to have_content 'メールアドレスメールアドレスを入力してください。'
+        expect(page).to have_content "ユーザー登録に失敗しました"
+        expect(current_path).to eq users_path
+      end
+    end
+
+    context '登録済のメールアドレスを使用' do
+      fit 'ユーザーの新規作成が失敗する' do
+        existed_user = FactoryBot.create(:user)
+        visit new_user_path
+        fill_in '名前', with: 'user_1'
+        fill_in 'メールアドレス', with: existed_user.email
+        fill_in 'パスワード', with: 'パスワード'
+        fill_in 'パスワード（確認）', with: 'パスワード'
+        click_button '新規登録'
+        expect(page).to have_content 'メールアドレスはすでに存在します'
+        expect(page).to have_content 'ユーザー登録に失敗しました'
+        expect(current_path).to eq users_path
+        expect(page).to have_field 'メールアドレス', with: existed_user.email
+      end
+    end
+  end
+end
